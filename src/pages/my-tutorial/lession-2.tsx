7aa96b9ed3e53a53d7b4ab9a5d0dec69a845565b
Markus Weskott <markus.weskott.privat@gmail.com>
2022-12-18 16:55:25 +0100

    feat: Gatsby tutorial lession 3

--- src/pages/my-tutorial/lession-2.tsx
+++ src/pages/my-tutorial/lession-2.tsx
@@ -1,4 +1,5 @@
 import { HeadFC, Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 import * as React from "react"
 import Layout from "../../components/layout"
 
@@ -6,6 +7,14 @@ export default function Component () {
   return (
     <Layout pageTitle={"My Gatsby Tutorial - Lession 2"}>
         <div>Page content of lession.</div>
+        <StaticImage
+            alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+            src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        />        
+        <StaticImage
+            alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+            src="../../images/dog.webp"
+        />        
     </Layout>
   )
 }
